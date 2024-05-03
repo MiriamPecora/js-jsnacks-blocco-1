8c81fc36329149f07d23de3e047d8eb07dda1f47
MiriamPecora <miriampecora97@gmail.com>
2024-05-03 12:28:09 +0200

    [FIXED]

--- JSnack2/script.js
+++ JSnack2/script.js
@@ -1,9 +1,22 @@
 let firstWord = prompt("Inserisci la prima parola");
 let secondWord = prompt("Inserisci la seconda parola");
 
+// if (firstWord.length < secondWord.length) {
+//     console.log(firstWord + " " + secondWord);
+// }
+// else {
+//     console.log(secondWord + " " + firstWord);
+// }
+
+// Con fix post correzione in classe:
 if (firstWord.length < secondWord.length) {
-    console.log(firstWord + " " + secondWord);
+    console.log(firstWord + " è una parola più corta di " + secondWord);
 }
+
+else if (firstWord.length > secondWord.length) {
+    console.log(secondWord + " è una parola più corta di " + firstWord);
+}
+
 else {
-    console.log(secondWord + " " + firstWord);
+    console.log("Le parole inserite hanno la stessa lunghezza");
 }
